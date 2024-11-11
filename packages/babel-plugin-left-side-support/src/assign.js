48c778ebc9bb14bf6e8ecbdd35714e8b6658e699
Pablo Santana Gonzalez <alu0101480541@ull.edu.es>
2024-11-11 22:51:09 +0000

    Fixed an issue with the transformation plugin, moved the support files to their previous directory and added a few lines in the test

---
+++ packages/babel-plugin-left-side-support/src/assign.js
@@ -0,0 +1,28 @@
+const { functionObject } = require('./function-object');
+
+function assign(f, cacheArgs, cacheValue) {
+    //console.log('f', f.toString(), cacheArgs, cacheValue);
+    //debugger;
+    for(let i = 0; i < cacheArgs.length; i++) {
+        let cacheArgument = cacheArgs[i];
+        if (cacheArgument == null) {
+            let error = new Error('Invalid null argument on left side of assignment');
+            throw error;
+        }
+        let next = i+1;
+        if (next == cacheArgs.length) { // the end
+            if (!f.cache) { // warning f pointer modification
+                f = functionObject(f);
+            }
+            f.setCache(cacheArgument, cacheValue);
+            return cacheValue;
+        } 
+        // If there are more arguments 
+        let auxF = functionObject(f(cacheArgument));
+        f.setCache(cacheArgument, auxF);
+        //console.error(`assign f.cache["${cacheArgument}"] = ${auxF}`);
+        f = auxF;
+    }
+}
+
+module.exports = { assign };
