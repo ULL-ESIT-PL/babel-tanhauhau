d02cec928bd4870050cd96b194a3e542823f8bf1
Pablo Santana Gonzalez <alu0101480541@ull.edu.es>
2024-11-11 20:19:53 +0000

    Moved the left side plugin outside of packages/babel-parser

---
+++ packages/babel-plugin-left-side-plugin/src/plugin.js
@@ -0,0 +1,45 @@
+//const parser = require("./lib/index.js");
+const types = require('@babel/types');
+const template = require('@babel/template');
+const SUPPORT_TEMPLATE = template('const {assign, functionObject} = require("babel-plugin-left-side-support");');
+
+module.exports = function leftSidePlugin(babel) {
+  return {
+    //parserOverride(code, opts) {
+    //  return parser.parse(code, opts);
+    //},
+    visitor: {
+      AssignmentExpression(path) {
+        const node = path.node;
+        if (node.operator == "=" && node.left.type == "CallExpression") {
+          // This supposes that the callee is an ID and not a member expression nor another function.
+          const callee = node.left.callee;
+          const args = node.left.arguments;
+          const rvalue = node.right;
+          const assignArgs = [callee, ...args, rvalue];
+          const functionAssign = babel.types.identifier("assign");
+          path.replaceWith(babel.types.callExpression(functionAssign, assignArgs));
+        }
+      },
+      FunctionDeclaration(path) {
+        const node = path.node;
+        if (node.assignable) {
+          node.assignable = false;
+          const identifier = types.identifier("functionObject");
+          const funId = node.id;
+          node.id = null;
+          // Replace the FunctionDeclaration with FunctionExpression.
+          const funAsExpr = types.functionExpression(null, node.params, node.body);
+          const callExpression = types.callExpression(identifier, [funAsExpr]);
+          const varDeclarator = types.variableDeclarator(funId, callExpression);
+          path.replaceWith(types.variableDeclaration("const", [varDeclarator]));
+        }
+      },
+      Program(path) {
+        const node = path.node;
+        // Perhaps checking when it's actually needed? Write on exit if an assignable function was created.
+        node.body.unshift(SUPPORT_TEMPLATE);
+      }     
+   }
+  }
+}
